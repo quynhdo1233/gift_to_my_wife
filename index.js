3908d9a826f6b87a65f847144fbf528f977979ae
quynhdo1233 <88832802+quynhdo1233@users.noreply.github.com>
2021-10-20 21:54:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const btn1=document.querySelector('.btn');
+const btn2 =document.querySelector('.btn2');
+const s1=document.querySelector('.select1');
+const s2=document.querySelector('.select2');
+const audio=document.querySelector('audio');
+const line=document.querySelector('.runline');
+const img=document.querySelector('.base img');
+var kt=0;
+var a;
+btn1.onclick=function(){
+    btn1.style.display="none";
+    btn2.style.animation= 'run linear 3.5s forwards ';
+    btn2.style.display="inline-block";
+}
+s1.onclick=function(){
+    btn2.style.display="none";
+    audio.src='https://tainhac123.com/listen/yeu-la-cuoi-phat-ho-ft-x2x.sme954UUZsz0.html';
+    audio.autoplay='true',
+    kt=1;
+    img.style.animation='img linear 3s forwards'
+    img.style.display='block';
+    clearInterval(a);
+}
+function loadsong(){
+    audio.src='';
+    audio.autoplay='true';
+    kt=1;
+}
+s2.onclick=function(){
+    if(kt==0){
+        btn2.style.display='none';
+        a=setInterval(function(){
+        btn2.style.display='inline-block';
+            if(kt==1){
+            btn2.style.display='none';
+            clearInterval(a);
+            }
+        }, 2000);
+        line.innerText='Sao em lại chọn "No", Chọn lại đi nàng ơi';
+    }
+}
+img.addEventListener('click',function(){
+    img.src='./asset/src/img/letter 2.jpg'
+})
+
